9742c9de781fcf7cc800bc3db610cc63dad59368
jakyoung <wkrud203@gmail.com>
2021-01-28 22:32:58 +0900

    no message

--- lib/screens/fourth.dart
+++ lib/screens/fourth.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:fluttertest/widgetUI/widget_ui_demo.dart';
 import 'package:flutter/foundation.dart';
 
@@ -16,8 +17,44 @@ class _SecondState extends State<Fourth> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: WidgetUIDemo(),
+    return Center(
+      child: RaisedButton(
+        child: Text(
+          'Show Pop-up',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        color: Colors.black,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => _buildPopupDialog(context),
+          );
+        },
+      ),
     );
   }
+}
+
+Widget _buildPopupDialog(BuildContext context) {
+  return new AlertDialog(
+    title: const Text('Popup example'),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Hello"),
+      ],
+    ),
+    actions: <Widget>[
+      new FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: const Text('닫기'),
+      ),
+    ],
+  );
 }
